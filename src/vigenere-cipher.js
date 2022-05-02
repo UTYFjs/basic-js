0dfd728c1bb5dbb5a926b32d279416613acd7955
utyfjs <belarus1990@gmail.com>
2022-05-03 02:44:14 +0400

    feat: add 1 task test

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
+  constructor(machine = true){
+    this.machine = machine;
+  }
+  encrypt(message, key) {
+    if (message === undefined || key === undefined){
+      throw new Error(`Incorrect arguments!`)
+    }
+    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    let arr =str.split();
+    let mess = message.toUpperCase();
+    let arrMessage = mess.split();
+    let arrKey = key.split();
+
+
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
+  decrypt(message, key) {
+    if (message === undefined || key === undefined){
+      throw new Error(`Incorrect arguments!`)
+    }
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 }
@@ -33,3 +49,37 @@ class VigenereCipheringMachine {
 module.exports = {
   VigenereCipheringMachine
 };
+
+
+/*![Ciphering machine](https://live.staticflickr.com/1931/44960892745_471bee66bb_b.jpg)  
+Криптография — это здорово! Давайте попробуем наладить производство шифровальных машин. Наши машины будут использовать один из методов шифрования, которые легки для понимания, но не могут быть разгаданы посредством простого криптоанализа — [**шифр Виженера**](https://en.wikipedia.org/wiki/Vigen%C3%A8re_cipher).
+
+Наша машина будет иметь 2 модификации: **прямая** и **обратная** (тип машины определяется в момент создания). **Прямая** машина просто шифрует и дешифрует строку, переданную в нее, а **обратная** машина возвращает **перевернутую** задом наперед строку после шифрования и дешифрования.
+
+Ваша задача — реализовать класс `VigenereCipheringMachine`. `constructor` этого класса принимает `true` (**или ничего**), чтобы создать **прямую** машину и `false`, чтобы создать **обратную** машину.
+Каждый экземляр `VigenereCipheringMachine` должен иметь 2 метода: `encrypt` и `decrypt`.
+
+Метод `encrypt` принимает 2 параметра: `message` (строка, чтобы ее зашифровать) и `key` (строку-кодовое слово).
+
+Метод `decrypt` принимает 2 параметра: `message` (строка, чтобы ее расшифровать) и `key` (строку-кодовое слово)
+
+Эти параметры для обоих методов являются **обязательными**. Если хотя бы один из них не был передан, должна быть выброшена ошибка с сообщением `Incorrect arguments!`. Машины шифруют и дешифруют **только символы латинского алфавита** (другие символы не изменяются). Строка, возвращаемая этими методами, должна иметь **верхний регистр**.
+
+Вам не нужно валидировать значение, переданное в `contructor` и в методы `encrypt` и `decrypt` (за исключением выбрасывания ошибки при отсутствии аргумента для для этих методов).
+
+Например:
+
+`const directMachine = new VigenereCipheringMachine();`
+
+`const reverseMachine = new VigenereCipheringMachine(false);`
+
+`directMachine.encrypt('attack at dawn!', 'alphonse')` => `'AEIHQX SX DLLU!'`
+
+`directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse')` => `'ATTACK AT DAWN!'`
+
+`reverseMachine.encrypt('attack at dawn!', 'alphonse')` => `'!ULLD XS XQHIEA'`
+
+`reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse')` => `'!NWAD TA KCATTA'`
+
+Напишите свой код в `src/vigenere-cipher.js`.
+ */
